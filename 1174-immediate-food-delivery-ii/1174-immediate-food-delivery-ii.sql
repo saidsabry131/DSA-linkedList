cff22117bb3d1bc60dfa7c4091aae240520a771e
said sabry <120335918+saidsabry131@users.noreply.github.com>
2026-02-17 14:15:14 +0200

    Time: 558 ms (89.11%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,8 +1,2 @@
-with first_orders as (
-    select * from delivery
-             where (order_date,customer_id)
-             in (select  min(order_date),customer_id from delivery group by customer_id)
-)
-
-
-select round( avg( first_orders.order_date = first_orders.customer_pref_delivery_date ) *100 ,2)as  immediate_percentage  from first_orders;
+select round(avg(tb.order_date = tb.customer_pref_delivery_date )*100,2)  as immediate_percentage  from (
+select * , row_number() over (partition by delivery.customer_id order by delivery.order_date) as rn from delivery )as tb where rn =1;
