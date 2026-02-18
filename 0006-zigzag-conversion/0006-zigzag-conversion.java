550ec820397398282f24bebaf2b74d7e36ec9918
said sabry <120335918+saidsabry131@users.noreply.github.com>
2026-02-18 11:19:36 +0200

    Time: 3 ms (94.24%), Space: 46.5 MB (88.73%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1) return s;
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i<numRows ; i++)
+        {
+            for (int j = i ; j<s.length(); j+=(2* (numRows -1)))
+            {
+                res.append(s.charAt(j));
+                if(i>0 && i<numRows - 1 && j+(2* (numRows -1))-(2*i) <s.length() )
+                {
+                    res.append(s.charAt(j+(2* (numRows -1))-(2*i)));
+                }
+            }
+        }
+
+        return res.toString();
+    }
+}
